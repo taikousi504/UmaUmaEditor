e8a2490049ef380ca79fc17f9ae7dd47044c5eec
yume1610rein <taikousi504@gmail.com>
2022-08-01 22:42:33 +0900

    自動起動

--- UmaUmaUpdater/Program.cs
+++ UmaUmaUpdater/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
 
 namespace UmaUmaUpdater
 {
@@ -6,7 +9,14 @@ namespace UmaUmaUpdater
     {
         static void Main(string[] args)
         {
+            WebClient wc = new WebClient();
+            wc.DownloadFile("https://raw.githubusercontent.com/taikousi504/UmaUmaEditor/master/UmaUmaUpdater/UmaMusumeLibrary.json", "../UmaLibrary/UmaMusumeLibrary.json");
+            wc.Dispose();
 
+            Console.WriteLine("UmaMusumeLibrary.jsonを更新しました。");
+
+            //UmaUmaCruise起動
+            Process.Start("../UmaUmaCruise.exe");
         }
     }
 }
